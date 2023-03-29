823c04b802612eb0e710c21428f7bc41d55d4072
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-03-29 14:33:49 +0300

    fix(navbar): fix prettier bug in remote merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-      primary: {
-        'orange' :'#FDA855',
-        'blue': '#0180AB',
-    
-      },
-  },
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    orange: "#FDA855",
+                    blue: "#0180AB",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
