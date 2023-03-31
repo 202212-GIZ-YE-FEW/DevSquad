3a83c33d79662ee5b63686b8b62983ac14050dba
fatGubari <fatimajubary@gmail.com>
2023-04-01 02:34:36 +0300

    feat(signup): fix problem

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 import Layout from "@/layout/Layout";
 
@@ -18,7 +19,7 @@ export default function HomePage() {
                 <Link href='/' locale='ar'>
                     العربية
                 </Link>
-            </div> */}
+            </div>
         </Layout>
     );
 }
