b444880414aa3c0f76595c19e9198f73e77a23f0
fatGubari <fatimajubary@gmail.com>
2023-04-01 03:18:57 +0300

    feat(signin): try to fix test problem

--- src/components/SignIn/__test__/SignIn.test.js
+++ src/components/SignIn/__test__/SignIn.test.js
@@ -1,6 +1,6 @@
 import renderer from "react-test-renderer";
 
-import SignIn from "../Index";
+import SignIn from "../index";
 
 it("renders correctly", () => {
     const tree = renderer.create(<SignIn />).toJSON();
