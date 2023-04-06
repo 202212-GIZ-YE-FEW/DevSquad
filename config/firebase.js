6485f333a4c528ec1a24128f22f780d56f8cd6d7
Ramiz Ali <ramiz.apk7@gmail.com>
2023-04-07 02:34:39 +0300

    build(config): add Firebase and Google map Configuration file and setup related packages

---
+++ config/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_DB_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_DB_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_DB_FPROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_DB_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_DB_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_DB_APPID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+//
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
