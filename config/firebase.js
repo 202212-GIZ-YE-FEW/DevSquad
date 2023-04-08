7e7ff4f59f641f34b58d7f92ce40440d2b8074b7
Ramiz Ali <ramiz.apk7@gmail.com>
2023-04-08 22:53:26 +0300

    fix(signin & signup): fix Prettier  error

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, TwitterAuthProvider } from "firebase/auth";
+import {
+    getAuth,
+    GoogleAuthProvider,
+    TwitterAuthProvider,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
