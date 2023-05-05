48a1d35d0e93956bd1989c36fb383427cacf0728
fatGubari <fatimajubary@gmail.com>
2023-05-05 14:41:08 +0300

    fix(update pagination and profile): update pagination and profile

--- src/components/Calendar/CalendarComponent/index.jsx
+++ src/components/Calendar/CalendarComponent/index.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function CalendarComponent({ day, year, month, date }) {
     const [isChecked, setIsChecked] = useState(false);
     let selectedDate = [];
+    // add 0 to the from 1 to 9 for filtering
     if (month <= 9) {
         month = "0" + month;
     }
@@ -12,6 +13,7 @@ export default function CalendarComponent({ day, year, month, date }) {
         day = "0" + day;
     }
     function handleDayClick() {
+        // store the select date and send it to event list for filtring
         selectedDate.push(`${year}-${month}-${day}`);
         date(selectedDate);
     }
--- src/components/PaginationComponent/paginate.jsx
+++ src/components/PaginationComponent/paginate.jsx
@@ -1,4 +1,6 @@
 export default function paginate(items, pageNumber, pageSize) {
+    // get the pageNumber muins one and multiple by the page size 2 it will be 1-1 =0 *2 =0
     const startIndex = (pageNumber - 1) * pageSize;
+    // get the events from the index 0 to the index 2 whitch will take to events and so on for the rest of events
     return items.slice(startIndex, startIndex + pageSize);
 }
