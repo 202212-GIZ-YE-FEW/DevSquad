8585609be9c899129280fda350e9af638bfec43f
Ramiz Ali <ramiz.apk7@gmail.com>
2023-05-07 08:32:58 +0300

    fix(dialogbox): fix error

--- src/components/DialogBox/__test__/DialogBox.test.js
+++ src/components/DialogBox/__test__/DialogBox.test.js
@@ -1,6 +1,6 @@
 import renderer from "react-test-renderer";
 
-import DialogBox from "..";
+import DialogBox from "../index";
 
 it("renders correctly", () => {
     const tree = renderer.create(<DialogBox />).toJSON();
